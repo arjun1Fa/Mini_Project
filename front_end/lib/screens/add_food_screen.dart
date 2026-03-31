f177a105d6248066a5fbeb835a823eaf6df1f0a6
winternFx <culerarjun@gmail.com>
2026-03-31 17:33:23 +0530

    feat: implement manual food entry screen with API integration and real-time nutritional preview

--- front_end/lib/screens/add_food_screen.dart
+++ front_end/lib/screens/add_food_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
